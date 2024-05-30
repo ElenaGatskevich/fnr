2870f10cd9b20cf1318c90c1aef36a3517f58c8a
ElenaGatskevich <45990899+ElenaGatskevich@users.noreply.github.com>
2024-05-30 13:27:30 +0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,19 +10,29 @@ document.addEventListener("DOMContentLoaded", (function () {
     }
 
 ));
-const social=document.getElementsByClassName('header__social')[0];
+const social=document.querySelectorAll('.header__social');
 
 const search=document.querySelectorAll('.search__input');
 search.forEach(el=>el.addEventListener('click',function (el) {
     this.closest('div').classList.add('open');
-    social.classList.add('hidden');
+    social.forEach(function (el) {
+       el.classList.add('hidden');
+
+
+
+    },false);
 
 
 },false));
 const searchButton=document.querySelectorAll('.search__close');
 searchButton.forEach(el=>el.addEventListener('click',function (el) {
     this.closest('.search').classList.remove('open');
-    social.classList.remove('hidden');
+    social.forEach(function (el) {
+        el.classList.remove('hidden');
+
+
+
+    },false);
 },false));
 
 const searchMobile=document.querySelectorAll('.search_mobile__button');
@@ -35,6 +45,15 @@ searchMobile.forEach(el=>el.addEventListener('click',function (el) {
 
 const mobileClose=document.querySelectorAll('.mobile__close');
 mobileClose.forEach(el=>el.addEventListener('click',function (el) {
-    const mobileBurger=document.getElementsByClassName('header__checkbox')[0];
+    const mobileBurger=document.querySelectorAll('.header__checkbox');
     mobileBurger.checked=0;
+    mobileBurger.forEach(function (el) {
+        el.checked=0;
+
+
+
+    },false);
 },false));
+function headfunc(val1,val2) {
+    return this;
+}
